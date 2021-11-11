4d6c925aa235ebac3a3f1a307dec6f386cdfc818
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-11 15:38:40 -0700

    Generate key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,19 @@
+require 'simplecov'
+
+SimpleCov.start
+
 class Enigma
-end 
+  attr_reader :key
+  def initialize
+    @key = generate_key
+  end
+
+  def generate_key
+    num = []
+    5.times do
+      num.push(rand(0..9))
+    end
+    num.join
+  end
+
+end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe Enigma do
     expect(key).to be_a(Enigma)
   end
 
+  it 'key is exactly 5 digits' do
+    num = Enigma.new
+
+    expect(num.key.length).to eq(5)
+  end
+
 end 
