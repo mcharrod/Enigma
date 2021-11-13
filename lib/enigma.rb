f9d558a6a453ae2dd6205cf5c8231cd2645c92a9
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-13 11:28:31 -0700

    Delegate objects to new classes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,25 +1,50 @@
 require 'simplecov'
 SimpleCov.start
 require 'date'
-require_relative 'values_module'
+require 'pry'
+require_relative 'key'
 
 class Enigma
-  include ValuesModule
 
-  attr_reader :key,
-              :abcd,
-              :date,
-              :offset
+  attr_reader :default_key
 
   def initialize
-    @key    = generate_key
-    @abcd   = key_letters
-    @date   = today
-    @offset = the_offset
+    @default_key = Key.new
   end
 
+  def encrypt(message, key = default_key, date = today)
+    binding.pry
+  end
+
+  #
+  # def generate_key
+  #   num = []
+  #   5.times do
+  #     num.push(rand(0..9))
+  #   end
+  #   @key = num.join
+  # end
+  #
+  # def key_letters
+  #   key_letters = {}
+  #   a = key[0..1]
+  #   b = key[1..2]
+  #   c = key[2..3]
+  #   d = key[3..4]
+  #   key_letters[:a] = a
+  #   key_letters[:b] = b
+  #   key_letters[:c] = c
+  #   key_letters[:d] = d
+  #   key_letters
+  # end
 
 
-
-
+  def forward_shift
+    the_shift = {}
+    the_shift[:a_shift] = the_offset[:a].to_i + key_letters[:a].to_i
+    the_shift[:b_shift] = the_offset[:b].to_i + key_letters[:b].to_i
+    the_shift[:c_shift] = the_offset[:c].to_i + key_letters[:c].to_i
+    the_shift[:d_shift] = the_offset[:d].to_i + key_letters[:d].to_i
+    the_shift
+  end
 end
