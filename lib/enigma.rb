88b2cf0b1c5afcad29574bf44f79eafb4ad92fc2
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-14 22:09:19 -0700

    Line up tabbing and method line order

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -27,27 +27,27 @@ class Enigma
     encrypted = []
       chunked(message.downcase).each do |chunk|
         encrypted.push(forward_rotate(chunk[0], @shifts[:a_shift]))
-          encrypted.push(forward_rotate(chunk[1], @shifts[:b_shift]))#bad
-            encrypted.push(forward_rotate(chunk[2], @shifts[:c_shift]))
-              encrypted.push(forward_rotate(chunk[3], @shifts[:d_shift]))
+        encrypted.push(forward_rotate(chunk[1], @shifts[:b_shift]))
+        encrypted.push(forward_rotate(chunk[2], @shifts[:c_shift]))
+        encrypted.push(forward_rotate(chunk[3], @shifts[:d_shift]))
       end
-    @return_gibberish[:encryption] = encrypted.compact.flatten.join
+    encrypted.compact.flatten.join
   end
 
   def decryptor(gibberish)
     decrypted = []
       chunked(gibberish).each do |chunk|
         decrypted.push(reverse_rotate(chunk[0], @shifts[:a_shift]))
-          decrypted.push(reverse_rotate(chunk[1], @shifts[:b_shift]))
-            decrypted.push(reverse_rotate(chunk[2], @shifts[:c_shift]))
-              decrypted.push(reverse_rotate(chunk[3], @shifts[:d_shift]))
+        decrypted.push(reverse_rotate(chunk[1], @shifts[:b_shift]))
+        decrypted.push(reverse_rotate(chunk[2], @shifts[:c_shift]))
+        decrypted.push(reverse_rotate(chunk[3], @shifts[:d_shift]))
       end
     decrypted.join
   end
 
   def forward_rotate(character, the_shift)
     if character != nil
-      if  @chars.include?(character) == false
+      if @chars.include?(character) == false
         character
       else
         character_ranking = @chars.index(character)
@@ -63,43 +63,32 @@ class Enigma
         character
       else
         char_index = @chars.index(character)
-          total = char_index - shift
+        total = char_index - shift
         @chars.rotate(total).first
       end
     end
   end
 
+
   def encrypt(message, key = generate_key, date = today)
     @key = Key.new(key)
     @offset = Offset.new(date)
+    total_shift
     @return_gibberish[:key] = key
     @return_gibberish[:date] = date
-    total_shift
-    scrambler(message)
+    @return_gibberish[:encryption] = scrambler(message)
     @return_gibberish
   end
 
   def decrypt(message, key, date)
     @key = Key.new(key)
     @offset = Offset.new(date)
-     total_shift
+    total_shift
     gibberish = message
     @return_message[:key] = key
     @return_message[:date] = date
-    # decrypted_message = message
-    # gibberish = decrypted_message.delete(:encryption)
-    # decrypted_message[:decryption] =
     @return_message[:decryption] = decryptor(gibberish)
     @return_message
-    # decrypted_message
-  end
-
-  def total_shift
-     @shifts[:a_shift] = (@key.a_key.to_i + @offset.a_offset.to_i)
-     @shifts[:b_shift] = (@key.b_key.to_i + @offset.b_offset.to_i)
-     @shifts[:c_shift] = (@key.c_key.to_i + @offset.c_offset.to_i)
-     @shifts[:d_shift] = (@key.d_key.to_i + @offset.d_offset.to_i)
-     @shifts
   end
 
 end
