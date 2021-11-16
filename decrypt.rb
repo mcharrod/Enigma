1f010e7ea4dd0a5700c4d471377c162b827c8dfb
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-16 00:56:35 -0700

    Adjust command line interface

--- decrypt.rb
+++ decrypt.rb
@@ -4,21 +4,20 @@ require './lib/key'
 require './lib/offset'
 
 enigma = Enigma.new
-enigma.encrypt("hello world", "02715", "040895")
+enigma.encrypt("hello world", "87092", "102715")
 
 
 
 if ARGV.count != 4
-  puts "invalid input"
+  puts "invalid input. Please enter your encrypted message file, the file you'd like to store your" + "\n"
+   "decrypted message, the key, and date."
 else
-  puts "Enter your encrypted message file, the file you'd like to store your" + "\n"
-   "decrypted message, the key, and date." 
-  date = ARGV[3]
   key  = ARGV[2]
+  date = ARGV[3]
   message = File.open(ARGV[0], "r")
   message_contents = message.read
   message.close
   plain_text = enigma.decrypt(message_contents, key, date)
   File.write(ARGV[1], plain_text[:decryption])
-  puts "created file #{ARGV[1]} with your decrypted message with the key #{enigma.key.key} and the date #{enigma.offset.date}"
+  puts "created file #{ARGV[1]} with your decrypted message with the key #{plain_text[:key]} and the date #{plain_text[:date]}"
 end
--- encrypt.rb
+++ encrypt.rb
@@ -6,7 +6,8 @@ require './lib/offset'
 enigma = Enigma.new
 
 if ARGV.count != 2
-  puts "invalid input"
+  puts "invalid input. Please enter the file your message is stored and \n" +
+  "the file you'd like to store your encrypted message in."
 else
     # Ruby allowed to read file
   message = File.open(ARGV[0], "r")
@@ -15,10 +16,10 @@ else
     # move to next part unrelated to reading file
   message.close
     # call the encrypt method to translate message to gibberish
-  gibberish = enigma.encrypt(message_contents, "02715", "102715")
+  gibberish = enigma.encrypt(message_contents, "87092", "102715")
     # write the contents of that gibberish message to the second file
   File.write(ARGV[1], gibberish[:encryption])
   puts "created a 'secure file' #{ARGV[1]} with your 'encrypted message'."
-  puts "your key is #{enigma.key.key}."
-  puts "your offset is #{enigma.offset.date}"
+  puts "your key is #{gibberish[:key]}."
+  puts "date is #{gibberish[:date]}"
 end
