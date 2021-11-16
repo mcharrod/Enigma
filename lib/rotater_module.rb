c98107519180b0a8666ee5222098f0e28e794b28
Katy Harrod <m.catherine.harrod@gmail.com>
2021-11-15 22:25:27 -0700

    Refactor rotate into one method

--- lib/rotater_module.rb
+++ lib/rotater_module.rb
@@ -14,38 +14,29 @@ module Rotater
       [:a_shift, :b_shift, :c_shift, :d_shift].each_with_index do |shift, index|
         next if chunk[index].nil?
 
-        string << forward_rotate(chunk[index], @shifts[shift])
+        string << rotate(chunk[index], @shifts[shift])
       end
     end
   end
 
-  # take the gibberish and translate to a language 
+  # take the gibberish and translate to a language
   def decryptor(gibberish)
     chunked(gibberish).each_with_object("") do |chunk, string|
       [:a_shift, :b_shift, :c_shift, :d_shift].each_with_index do |shift, index|
         next if chunk[index].nil?
 
-        string << reverse_rotate(chunk[index], @shifts[shift])
+        string << rotate(chunk[index], -@shifts[shift])
       end
     end
   end
 
-  # take each character and obscure it by that character's shift
-  def forward_rotate(character, shift)
+  # take each character and rotate it by that character's shift
+  def rotate(character, shift)
     return character unless @character_array.include?(character)
 
     character_index = @character_array.index(character)
     total = character_index + shift
     @character_array.rotate(total).first
-  end
-
-  # take each character and revert it back to it's original character
-  def reverse_rotate(character, shift)
-    return character unless @character_array.include?(character)
-
-      character_index = @character_array.index(character)
-      total = character_index - shift
-      @character_array.rotate(total).first
   end
 
 end
